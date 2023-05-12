1ed6c24ea8dcdd8936e90e2beb48be155299d45d
tkennelly <theresa.a.kennelly@gmail.com>
2023-05-12 12:24:36 -0400

    lab

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 // Write your DOM code here!
+let red = document.querySelector('.red')
+
+red.addEventListener('click', () => {
+    let body = document.querySelector('body')
+    body.style.backgroundColor = 'red'
+})
+
+let white = document.querySelector('.white')
+
+white.addEventListener('click', () => {
+    let body = document.querySelector('body')
+    body.style.backgroundColor = 'white'
+})
+
+let blue = document.querySelector('.blue')
+
+blue.addEventListener('click', () =>  {
+    let body = document.querySelector('body')
+    body.style.backgroundColor = 'blue'
+})
+
+let yellow = document.querySelector('.yellow')
+
+yellow.addEventListener('click', () => {
+    let body = document.querySelector('body')
+    body.style.backgroundColor = 'yellow'
+})
